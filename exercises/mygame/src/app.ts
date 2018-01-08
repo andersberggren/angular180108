0bf0389bbf62b2a32689b1dcf6db473c323139ce
Anders Berggren <anders.berggren@acino.se>
2018-01-08 10:49:18 +0100

    My game, copied from template

---
+++ exercises/mygame/src/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <h1>Title!</h1>
+    <p>Add your template code here! :)<p>
+  `,
+  styles: []
+})
+export class AppComponent {}
